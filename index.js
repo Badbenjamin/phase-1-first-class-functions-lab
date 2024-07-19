4fc63306977f26dd0c16af4621baaa1eba424811
ben mccarthy <bmccarthy90@gmail.com>
2024-07-19 16:25:42 -0400

    passins 1st class functions lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+// 1
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function (array) {
+   return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function (array) {
+    return drivers.slice(drivers.length - 2, drivers.length);
+}
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+    
+
+
+
+
+const createFareMultiplier = function (fare, func) {
+    return function multiplyFare(fare) {
+        return fare * fare;
+    }
+}
+
+const fareDoubler = function (fare) {
+    return fare * 2;
+}
+
+const fareTripler = function (fare) {
+    return fare * 3;
+}
+
+const selectDifferentDrivers = function (array, daFunction) {
+    return daFunction(array);
+}
+
